3c11fe912bb4c4cfbd08fcb12ee66d01a10cb967
RasikaMadushanka <rasikamadushanka2005@gmail.com>
2026-02-18 11:21:46 +0530

    make slot service

--- src/main/java/edu/icet/ecom/Service/interviewSlot_service.java
+++ src/main/java/edu/icet/ecom/Service/interviewSlot_service.java
@@ -1,4 +1,10 @@
 package edu.icet.ecom.Service;
 
+import edu.icet.ecom.Model.Dto.interviewSlot_dto;
+
+import java.util.List;
+
 public class interviewSlot_service {
+    public List<interviewSlot_dto> getAllSlots() {
+    }
 }
